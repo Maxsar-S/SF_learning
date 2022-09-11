c229c3c47b64d82a8f3196289d460fd0b3282fd0
VladimirK <kavr17@mail.ru>
2022-09-11 11:44:09 +0600

    D16

--- bulletin_board/board/boardApp/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class BoardappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'boardApp'
-
-    def ready(self):
-        import boardApp.signals
